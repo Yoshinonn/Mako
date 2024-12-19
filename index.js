ad5c96d82cf51648c9f843284034338d6b371247
sarizaldy <71543740+sarizaldy@users.noreply.github.com>
2024-12-19 12:46:25 +0700

    Update UUID and CDN

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ import { connect } from 'cloudflare:sockets';
  * User configuration and settings
  * Generate UUID: [Windows] Press "Win + R", input cmd and run: Powershell -NoExit -Command "[guid]::NewGuid()"
  */
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'f21b7162-0004-4f66-95fd-372b6b1dd291';
 
 /**
  * Array of proxy server addresses with ports
  * Format: ['hostname:port', 'hostname:port']
  */
-const proxyIPs = ['cdn.xn--b6gac.eu.org:443', 'cdn-all.xn--b6gac.eu.org:443'];
+const proxyIPs = ['188.166.199.61:443', '128.199.218.226:443'];
 
 // Randomly select a proxy server from the pool
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
